2aa505f73ec34b0f2eaab5f2d393ec5d0bf0c8a1
yevgenlisovenko <yevgen.lisovenko@gmail.com>
2025-11-13 22:37:06 -0500

    Added ESLint and Prettier to backend and frontend

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,10 +2,11 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import prettier from 'eslint-plugin-prettier/recommended'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
@@ -24,6 +25,8 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'prettier/prettier': 'error',
     },
   },
+  prettier, // Must be last to override other configs
 ])
